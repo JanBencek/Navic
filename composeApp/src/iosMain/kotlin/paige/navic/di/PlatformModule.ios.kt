c89c3222acc94becbbe5ad691681aedc7d3a4650
paige <paigely@tuta.io>
2026-08-07 23:48:38 +0200

    explode

--- composeApp/src/iosMain/kotlin/paige/navic/di/PlatformModule.ios.kt
+++ composeApp/src/iosMain/kotlin/paige/navic/di/PlatformModule.ios.kt
@@ -61,6 +61,7 @@ actual val platformModule = module {
 	viewModel<MediaPlayerViewModel> {
 		IOSMediaPlayerViewModel(
 			stateRepository = get(),
+			songRepository = get(),
 			downloadManager = get(),
 			connectivityManager = get(),
 			syncManager = get(),
